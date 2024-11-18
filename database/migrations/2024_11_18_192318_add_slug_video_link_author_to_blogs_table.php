a31015c18cdc8c75243ee7f2115984f6ebd2219a
Akshay Sorathiya <akshaysorathiya555@gmail.com>
2024-11-19 00:55:48 +0530

    add blogs table columns slug,video_link,author

---
+++ database/migrations/2024_11_18_192318_add_slug_video_link_author_to_blogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+            $table->string('video_link')->nullable();
+            $table->string('author')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn('slug');
+            $table->dropColumn('video_link');
+            $table->dropColumn('author');
+        });
+    }
+};
