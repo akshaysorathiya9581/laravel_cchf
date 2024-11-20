eae2ac49a073069c467cc6540c56037d7070d6c0
Divyesh <divyeshbhanderi066@gmail.com>
2024-11-20 21:48:21 +0530

    header file changes

--- resources/views/frontend/templates/masbia-template/includes/header.blade.php
+++ resources/views/frontend/templates/masbia-template/includes/header.blade.php
@@ -6,9 +6,9 @@
   <meta name="viewport"
     content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>
-        @yield('title') - {{ config('app.name', 'Masbia') }}
-    </title>
+    
+    <x-open-graph-meta :page="($page ?? '')" :campaign="($campaign ?? '')" :ogcustommeta="($ogcustommeta ?? '')" />
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
