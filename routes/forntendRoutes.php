622a55f8d18619babdb38c270bd28f4a6a498217
Akshay Sorathiya <akshaysorathiya555@gmail.com>
2024-11-25 00:33:38 +0530

    route changes working on it

--- routes/forntendRoutes.php
+++ routes/forntendRoutes.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\MasbiaDonationController;
 use App\Http\Controllers\MasbiaBlogsController;
+use App\Http\Controllers\BillingController;
 use Illuminate\Support\Facades\Route;
 
 // Route::middleware('guest')->group(function () {
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
         return view('emails.donor-notification');
     });
     
-    Route::get('/campaign/{campaign}/{team?}', [FrontendController::class, 'raffle'])->name('raffle')->middleware('visitor');
+    Route::get('/{campaign}/{team?}', [FrontendController::class, 'raffle'])->name('raffle')->middleware('visitor')->where('campaign', 'donate|masbia1'); // Add constraints here;
     Route::get('/load-more-donations', [DonationController::class, 'loadMoreDonations']);
     Route::get('/load-more-teams', [DonationController::class, 'loadMoreTeams']);
     Route::get('/load-more-team-donations', [DonationController::class, 'loadMoreTeamDonations']);
@@ -26,9 +27,9 @@ use Illuminate\Support\Facades\Route;
     Route::post('/payment/{campaign}', [PaymentController::class, 'processPayment'])->name('payment.process');
     Route::post('/campaign/create_team', [FrontendController::class, 'create_team'])->name('campaign.create_team');
 
-    Route::get('masbia/donation', [MasbiaDonationController::class, 'index'])->name('donation.index');
-    Route::get('masbia/blogs', [MasbiaBlogsController::class, 'index'])->name('blogs.index');
-    Route::get('masbia/blog/{title}', [MasbiaBlogsController::class, 'view'])->name('blogs.view');
+    Route::get('donation', [MasbiaDonationController::class, 'index'])->name('donation.index');
+    Route::get('blogs', [MasbiaBlogsController::class, 'index'])->name('blogs.index');
+    Route::get('blog/{title}', [MasbiaBlogsController::class, 'view'])->name('blogs.view');
     Route::get('blogs/get-blogs', [MasbiaBlogsController::class, 'getBlogs'])->name('blogs.get-blogs');
 // });
 
@@ -37,6 +38,9 @@ Route::middleware('auth')->group(function () {
     Route::post('profileupdate', [ProfileController::class, 'update'])->name('profile.update');
     Route::get('notification', [NotificationController::class, 'edit'])->name('notification.edit');
     Route::post('notification-update', [NotificationController::class, 'update'])->name('notification.update');
+    Route::get('billing', [BillingController::class, 'index'])->name('profile.billing');
+    Route::get('donation/get-detail', [BillingController::class, 'donationDetail'])->name('donation.get');
+    Route::get('donation/get-data', [BillingController::class, 'getDonationList'])->name('donation.list');
 });
 
 // Route::domain('{organization}/{slug}')->group(function () {
